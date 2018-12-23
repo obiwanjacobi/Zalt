a978f015cc5e029f68263cb5cacf79e2eb2e3e2c
Marc Jacobi <obiwanjacobi@hotmail.com>
2018-12-23 10:14:54 +0100

    asm noodling

--- Source/Zalt Bios/_template.asm
+++ Source/Zalt Bios/_template.asm
@@ -1,3 +1,4 @@
+;; --------------------------------------------------------
 
 ; public function
 public component_object_function
@@ -9,8 +10,9 @@ public component_object_function
 component_object_function:
     xor a           ; comment
 	
+
+;; --------------------------------------------------------
 	
-;	
 ; example of a fast 16 bit loop
 ; de: contains the 16-bit loop length
 
@@ -24,8 +26,11 @@ component_object_function:
 	dec d			; decrement msb
 	jr nz, loop		; msb outer loop
 	
-	
-	
+
+
+
+
+;; --------------------------------------------------------------------------------------------------------------------
 ;; 	difference between sub-routines and services:
 ;; 	- sub-routines are called with native call-ret and pass arguments by documented registers.
 ;; 	- services are called (through a funtion table) with native call-ret but pass arguments by stack. 
@@ -33,13 +38,14 @@ component_object_function:
 
 ; example of parameter passing via the SP.
 ; parameters are always 16 bits (because SP works only with 16 bits values)
-; caller pushes parameters on the SP then call
+; caller pushes parameters on the SP (left-to-right)
+; caller then calls callee (call-ret)
 ; callee pops the parameters of the SP in reverse order
 ; callee needs to preserve return address
 ; caller cleans up parameters from stack after callee returns
 ; there is NO return value. Any return value must be passed as out-parameter
 
-
+caller:
 ; caller prepares parameters
 	push de
 	push hl
@@ -50,38 +56,61 @@ component_object_function:
 	pop de
 ; caller (optionally) use the popped values of out or ref parameters
 
-defc  method_parameter_offset	4	; saved ix and return address
-defc  method_localvar_offset	0	; ?value is specific per scope
+; stack on entry:
+;	- param 1 (de)
+;	- param 2 (hl)
+;	- ret-addr
+; sp =>
 
-; callee prolog: executed at the beginning of every service	
-	push ix			; save ix
-	ld ix, $0000
+callee:
+; callee prolog: executed at the beginning of every service	- with local vars
+	push ix			; save incoming ix
+	ld ix, 0		; prepare to copy sp into ix
 	add ix, sp		; ix now points to the bottom of the stack
+	ld hl, -4		; reserve space for the local vars (4 bytes)
+	add	hl, sp		; calc where the sp would end up
+	ld sp, hl		; move the stack pointer to keep local vars safe
 
-; callee body: retrieve parameters (ix with posative displacement) reversed little endian
-	ld h, (ix + method_parameter_offset)
-	ld l, (ix + method_parameter_offset + 1)
-	ld d, (ix + method_parameter_offset + 2)
-	ld e, (ix + method_parameter_offset + 3)
-	
-; callee local vars init - for all local vars (in scope)
-	ld      ix, -n          ; allocate n bytes for local variables
-    add     ix, sp
-    ld      sp, ix  	;!! THIS CHANGES SP (AND IX)! EPILOG WILL FAIL
-	
+; stack when prolog done (sp < ix):
+;	- param 1 (de)
+;	- param 2 (hl)
+;	- ret-addr
+; ix =>
+;	- var 1 (l)
+;	- var 1 (h)
+;	- var 2 (l)
+;	- var 2 (h)
+; sp =>
+
+
+defc params_offset = 4		; offset to jump over the pushed ix and return address on stack to get to params
+
+; callee body: retrieve parameters (ix with positive displacement) reversed little endian
+	ld h, (ix + params_offset + 0)	; a positive offset goes back up the stack
+	ld l, (ix + params_offset + 1)
+	ld d, (ix + params_offset + 2)
+	ld e, (ix + params_offset + 3)
+
+; callee body: write out parameters (ix with positive displacement) reversed little endian
+	ld (ix + params_offset + 0), h	; a positive offset goes back up the stack
+	ld (ix + params_offset + 1), l
+	ld (ix + params_offset + 2), d
+	ld (ix + params_offset + 3), e
+
+defc vars_offset = 0		; offset to jump to local vars area on stack
+
 ; callee uses local vars (ix with negative displacement) little endian
-	ld l, (ix - )
-	ld h, (ix - )
-
+	ld h, (ix - vars_offset - 0)	; a negative offset goes down the stack
+	ld l, (ix - vars_offset - 1)
+	ld d, (ix - vars_offset - 2)
+	ld e, (ix - vars_offset - 3)
 
 ; callee can call other services (callee becomes caller)
 	push hl
 	call subcallee
 	pop hl
 	
-	
 ; callee epilog: executed at the end of every service call
-	pop ix		; restore ix
+	ld	sp, ix	; restore the original sp
+	pop ix		; restore saved ix
 	ret
-	
-	
