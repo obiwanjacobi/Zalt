59e4726a877e9ae5332728207bccdf76ae3d2d05
Marc Jacobi <Marc.Jacobi@macaw.nl>
2018-12-23 19:38:45 +0100

    zalt_c lib

--- Source/z88dk_target/_tests/HelloWorld-printf/make.cmd
+++ Source/z88dk_target/_tests/HelloWorld-printf/make.cmd
@@ -1,7 +1,7 @@
 @ECHO OFF
 @SET PATH=%PATH%;C:\z88dk\bin\
 
-set zalt_sources=../../src/io/Stream.c ../../src/io/ConsoleStreamProvider.c ../../src/sys/Thread.c ../../src/sys/Async.c ../../src/sys/Error.c ../../src/sys/Heap.c ../../src/sys/String.c
+set zalt_lib=../../zalt_c.lib
 
 @REM zcc +zalt -clib=new -m helloworld.c -o helloworld.asm -a
 @REM zcc +zalt -clib=new -m helloworld.c -o helloworld --list
@@ -9,4 +9,4 @@ set zalt_sources=../../src/io/Stream.c ../../src/io/ConsoleStreamProvider.c ../.
 @REM zcc +zalt -clib=sdcc_iy --max-allocs-per-node200000 --c-code-in-asm -m helloworld.c -o helloworld.asm -a
 @REM zcc +zalt -clib=sdcc_iy --max-allocs-per-node200000 helloworld.c -o helloworld --list
 
-zcc +zalt -clib=sdcc_iy --max-allocs-per-node200000 helloworld.c %zalt_sources% -o helloworld --list
+zcc +zalt -clib=sdcc_iy --max-allocs-per-node200000 helloworld.c -l%zalt_lib% -o helloworld --list
--- Source/z88dk_target/build.cmd
+++ Source/z88dk_target/build.cmd
@@ -7,4 +7,8 @@ SET PATH=%PATH%;C:\z88dk\bin\
 @REM zcc +zalt -clib=sdcc_iy --max-allocs-per-node200000 -m @_zalt.lst -o zalt --list
 
 @REM zcc +zalt -vn -m -SO3 -DDEBUG -Ca-DDEBUG -clib=sdcc_iy --max-allocs-per-node200000 @_zalt.lst -o zalt
-zcc +zalt -m -SO3 -DDEBUG -Ca-DDEBUG -clib=sdcc_iy @_zalt.lst -o zalt
+
+:: make lib: zalt_c.lib
+::zcc +zalt -m -s --list -SO2 -DDEBUG -Ca-DDEBUG -clib=sdcc_iy @_zalt.lst -o zalt
+zcc +zalt -m -s --list -SO2 -DDEBUG -Ca-DDEBUG -clib=sdcc_iy @_zalt.lst -c
+z80asm --IXIY -xzalt_c -DDEBUG -D__SDCC -D__SDCC_IY @_zalt.o.lst
